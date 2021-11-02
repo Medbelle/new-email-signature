0bc4bbc0b8e598c99afcf379fe185df385f08b7b
Daniel Florez <danielflorez111@gmail.com>
2021-11-03 00:42:14 +0100

    Whatsapp number and Linkedin

--- app.js
+++ app.js
@@ -9,5 +9,16 @@ document.getElementById("email").innerHTML = email;
 document.getElementById("email").href = `mailto:${email}`;
 
 const phone = prompt("Phone: ");
-document.getElementById("phone").innerHTML = phone;
+document.getElementById("phone").innerHTML = `Tel. ${phone}`;
 document.getElementById("phone").href = `tel:${phone}`;
+
+const whatsapp = prompt("Whatsapp number (optional): ");
+const whatsappNumber = whatsapp && whatsapp.trim();
+
+if (whatsappNumber) {
+    document.getElementById("whatsapp").innerHTML = `Whatsapp. ${whatsapp}`;
+    document.getElementById("whatsapp").href = `https://wa.me/${whatsapp.replace(/\s/g, '')}`;
+} else {
+    document.getElementById("whatsapp").remove();
+    document.getElementById("whatsapp-br").remove();
+}
