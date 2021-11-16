59a84c948cd703255ea1cdd21ebc5b036ab34924
Daniel Florez <danielflorez111@gmail.com>
2021-11-16 12:03:17 +0100

    Test

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ if (whatsappNumber) {
     document.getElementById("whatsapp").innerHTML = `Whatsapp. ${whatsapp}`;
     document.getElementById("whatsapp").href = `https://wa.me/${whatsapp.replace(/\s/g, '')}`;
 } else {
-    document.getElementById("whatsapp").remove();
-    // document.getElementById("whatsapp-br").remove();
+    // document.getElementById("whatsapp").remove();
+    document.getElementById("whatsapp").innerHTML = '';
+    document.getElementById("whatsapp").href = '';
 }
